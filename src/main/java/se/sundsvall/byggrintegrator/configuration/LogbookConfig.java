4e5bb45ec8b11b477f692c5d6a08bf4cb2f5a85a
Martin Hansson <martinhansson80@gmail.com>
2025-01-21 09:49:31 +0100

    uf-11673: Introduce default excluded paths (temporarily)

--- src/main/java/se/sundsvall/byggrintegrator/configuration/LogbookConfig.java
+++ src/main/java/se/sundsvall/byggrintegrator/configuration/LogbookConfig.java
@@ -1,11 +1,15 @@
 package se.sundsvall.byggrintegrator.configuration;
 
+import static org.zalando.logbook.core.Conditions.exclude;
 import static se.sundsvall.dept44.logbook.filter.ResponseFilterDefinition.binaryContentFilter;
 import static se.sundsvall.dept44.logbook.filter.ResponseFilterDefinition.fileAttachmentFilter;
 import static se.sundsvall.dept44.util.EncodingUtils.fixDoubleEncodedUTF8Content;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,9 +17,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Correlation;
 import org.zalando.logbook.HttpLogWriter;
+import org.zalando.logbook.HttpRequest;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.Precorrelation;
 import org.zalando.logbook.core.BodyFilters;
+import org.zalando.logbook.core.Conditions;
 import org.zalando.logbook.core.DefaultSink;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 
@@ -25,22 +31,35 @@ public class LogbookConfig {
 	private static final String LOGGER_NAME = "se.sundsvall.dept44.payload";
 
 	private final int maxBodySizeToLog;
+	private final Set<String> excludedPaths;
 
-	public LogbookConfig(@Value("${logbook.logs.maxBodySizeToLog}") int maxBodySizeToLog) {
+	public LogbookConfig(
+		@Value("${logbook.default.excluded.paths}") Set<String> defaultExcludedPaths,
+		@Value("${logbook.logs.maxBodySizeToLog}") int maxBodySizeToLog) {
 		this.maxBodySizeToLog = maxBodySizeToLog;
+
+		excludedPaths = defaultExcludedPaths;
 	}
 
 	@Bean
 	Logbook logbook(final ObjectMapper objectMapper) {
 		return Logbook.builder()
+			.bodyFilter(BodyFilters.truncate(maxBodySizeToLog))
 			.sink(new DefaultSink(
 				new JsonHttpLogFormatter(objectMapper),
 				new NamedLoggerHttpLogWriter(LOGGER_NAME)))
 			.responseFilters(List.of(
 				fileAttachmentFilter(),
 				binaryContentFilter()))
-			.bodyFilter(BodyFilters.truncate(maxBodySizeToLog))
+			.condition(exclude(getExclusions()))
 			.build();
+	}
+
+	private List<Predicate<HttpRequest>> getExclusions() {
+		return Optional.of(excludedPaths).stream()
+			.flatMap(Set::stream)
+			.map(Conditions::requestTo)
+			.toList();
 	}
 
 	static class NamedLoggerHttpLogWriter implements HttpLogWriter {
